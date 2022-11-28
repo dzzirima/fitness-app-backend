8933dfb45fc0fa2c871c15c2a9b9aa6f4c542436
David T Zirima <52695278+dzzirima@users.noreply.github.com>
2022-11-28 15:43:23 +0200

    chor :last commit

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,4 +3,4 @@
 echo 'run application_start.sh: ' >> /home/ec2-user/apis/autodep/deploy.log
 
 echo 'pm2 restart' >> /home/ec2-user/apis/autodep/deploy.log
-pm2 restart nodejs-express-app >> /home/ec2-user/apis/autodep/deploy.log
+pm2 restart fitness >> /home/ec2-user/apis/autodep/deploy.log
