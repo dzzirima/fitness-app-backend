18203aeb3a9f3412c28c3b59c3e6da79e2bbdb3b
David T Zirima <52695278+dzzirima@users.noreply.github.com>
2022-11-28 23:56:11 +0200

    chor :testing auto deployment

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,7 +2,7 @@
 
 echo 'run application_start.sh: ' >> /home/ec2-user/apis/autodep/deploy.log
 
-echo 'pm2 restart' >> /home/ec2-user/apis/autodep/deploy.log
+echo 'pm2 restart !!!' >> /home/ec2-user/apis/autodep/deploy.log
 
-
+echo 'restarting fitness backend app ....!!' >> /home/ec2-user/apis/autodep/deploy.log
 # pm2 start dist/src/index.js --name=fitness_auto >> /home/ec2-user/apis/autodep/deploy.log
