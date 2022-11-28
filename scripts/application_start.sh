cf21ab1813c821d3a1ebe32c2152cceb917a5d4f
David T Zirima <52695278+dzzirima@users.noreply.github.com>
2022-11-29 00:46:50 +0200

    feat :manually startng the server

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -5,5 +5,5 @@ echo 'run application_start.sh: ' >> /home/ec2-user/apis/autodep/deploy.log
 echo 'pm2 restart !!!' >> /home/ec2-user/apis/autodep/deploy.log
 
 echo 'restarting fitness backend app ....!!' >> /home/ec2-user/apis/autodep/deploy.log
-pm2 restart 0 >>/home/ec2-user/apis/autodep/deploy.log
-# pm2 start dist/src/index.js --name=fitness_auto >> /home/ec2-user/apis/autodep/deploy.log
+# pm2 restart 0 >>/home/ec2-user/apis/autodep/deploy.log
+pm2 start dist/src/index.js --name=fitness-backend >> /home/ec2-user/apis/autodep/deploy.log
