dd83bbfb4263593e966ff3ae3a99c36859936495
David T Zirima <52695278+dzzirima@users.noreply.github.com>
2022-11-28 16:13:00 +0200

    chor : working code

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,4 +3,4 @@
 echo 'run application_start.sh: ' >> /home/ec2-user/apis/autodep/deploy.log
 
 echo 'pm2 restart' >> /home/ec2-user/apis/autodep/deploy.log
-npm run dev >> /home/ec2-user/apis/autodep/deploy.log
+pm2 start dist/src/index.js --name=fitness_auto >> /home/ec2-user/apis/autodep/deploy.log
