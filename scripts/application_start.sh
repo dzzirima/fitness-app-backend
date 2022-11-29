78388ca9e17531e6bc5edfff773694c4ef554660
David T Zirima <52695278+dzzirima@users.noreply.github.com>
2022-11-29 16:54:01 +0200

    chor :removed unnecessary files

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -5,9 +5,7 @@ echo 'run application_start.sh: ' >> /home/ec2-user/apis/autodep/deploy.log
 echo 'pm2 restart !!!' >> /home/ec2-user/apis/autodep/deploy.log
 
 echo 'restarting fitness backend app ....!!' >> /home/ec2-user/apis/autodep/deploy.log
-# # pm2 restart 0 >>/home/ec2-user/apis/autodep/deploy.log
-# pm2 start dist/src/index.js --name=fitness-backend >> /home/ec2-user/apis/autodep/deploy.log
-# npm run dev
+
 
 echo 'running server as  a background job ....!!' >> /home/ec2-user/apis/autodep/deploy.log
 nohup node  dist/src/index.js & >> /home/ec2-user/apis/autodep/deploy.log
