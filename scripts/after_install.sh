0c957ad407737822d202ae7d7215b390f910dfc1
David T Zirima <52695278+dzzirima@users.noreply.github.com>
2022-11-29 11:16:57 +0200

    chor :remved dist

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -7,11 +7,6 @@ cd /home/ec2-user/apis/autodep >> /home/ec2-user/apis/autodep/deploy.log
 echo 'npm install' >> /home/ec2-user/apis/autodep/deploy.log 
 npm install >> /home/ec2-user/apis/autodep/deploy.log
 
-echo 'building app now ....!' >> /home/ec2-user/apis/autodep/deploy.log 
-tsc
-
-echo 'copying env to dist....!' >> /home/ec2-user/apis/autodep/deploy.log 
-cp .env  ./dist/.env
 
 echo 'app ready for deployment' >> /home/ec2-user/apis/autodep/deploy.log 
 
