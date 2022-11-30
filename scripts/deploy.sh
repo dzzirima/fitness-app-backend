f3d9626e5f53e5178a2c402bed6e23e30595cbb3
David T Zirima <52695278+dzzirima@users.noreply.github.com>
2022-11-30 11:02:08 +0200

    running script

---
+++ scripts/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo 'deplyoying....!!' >> /home/ec2-user/apis/autodep/deploy.log
+cd /home/ec2-user/apis/autodep
+
+nohup bash -c "exec -a fitness node dist/src/index.js&" >> /home/ec2-user/apis/autodep/deploy.log
+
+
+
+
